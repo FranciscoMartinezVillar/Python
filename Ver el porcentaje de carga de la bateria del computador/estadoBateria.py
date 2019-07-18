f38b7a620aeaa1dfe74098fb5a32be42e93196af
FranciscoMartinezVillar <33935361+FranciscoMartinezVillar@users.noreply.github.com>
2019-07-18 13:25:40 +0200

    GestordeBateriaIFTTT.py
    He incluido la funcionalidad de que el propio script se ejecute de forma cíclica para supervisar el estado de la batería y que gestione un SmartPlug a través de IFTTT, de esta manera nos podemos olvidad de estar conectando y desconectando nuestro portátil mientras estamos trabajando.

--- Ver el porcentaje de carga de la bateria del computador/estadoBateria.py
+++ Ver el porcentaje de carga de la bateria del computador/estadoBateria.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# Nombre:       estadoBateria.py
-# Autor:        Miguel Andres Garcia Niño
+# Nombre:       GestordeBateriaIFTTT.py
+# Autor:        Miguel Andres Garcia Niño y Francisco Martinez Villar
 # Creado:       18 de Mayo 2018
 # Modificado:   18 de Mayo 2018
 # Copyright:    (c) 2018 by Miguel Andres Garcia Niño, 2018
 # License:      Apache License 2.0
 # ----------------------------------------------------------------------------
 
-__versión__ = "1.0"
+#__versión__ = "1.0"
 
 """
-El módulo *estadoBateria* permite ver el porcentaje de carga de la batería y si
+El módulo *GestordeBateria* permite ver el porcentaje de carga de la batería y si
 está recibiendo carga.
+ Francisco Martinez añade la funcionalidad de control de un SmartPlug a traves de un
+ Webhook de IFTTT, para saber como usarlo ir a  https://ifttt.com/maker_webhooks
+
 """
 
 from ctypes import Structure, wintypes, POINTER, windll, pointer, WinError
-
-
+# importing time library 
+import time
+# importing the requests library 
+import requests
 # ===================== FUNCIÓN cargaBateria =======================
 
 def cargaBateria():
@@ -48,17 +53,23 @@ def estadoBateria():
    cargadorCarga = cargaBateria()
    cargador = cargadorCarga[0]
    carga = cargadorCarga[1]
-   
+   maxNivel = 80
+   minNivel = 30
    if cargador == 0:
       print("La batería no esta recibiendo carga.")
-      if carga <= 15:
-         print("La carga de la batería está por debajo o igual al 15%.")
+      if carga <= minNivel:
+         print("La carga de la batería está por debajo o igual al {}%, conectando el SmartPlug".format(minNivel))
+         r = requests.get('https://maker.ifttt.com/trigger/{evento que conecta}/with/key/{yours key}')
       else:
          print("Carga de la batería: {}%.".format(carga))
+         
    elif cargador == 1:
       print("La batería esta recibiendo carga.")
-      if carga >= 95:
-         print("La carga de la batería está por encima o igual al 95%.")
+      if carga >= maxNivel:
+         print("La carga de la batería está por encima o igual al {}%, desconectando el SmartPlug".format(maxNivel))
+         r = requests.get('https://maker.ifttt.com/trigger/{evento que desconecta}/with/key/{yours key}')
+ 
+         
       else:
          print("Carga de la batería: {}%.".format(carga))
    else:
@@ -66,5 +77,11 @@ def estadoBateria():
 
 
 # ======================== LLAMAR FUNCIÓN ==========================
+# Bucle infinito gestionando la carga y descarga del ordenador
+# Envia un webhooks a IFTTT y conecta y desconecta el SmartPlug
 
-estadoBateria()
+while True:
+    estadoBateria()
+    # Wait for 5 seconds
+    time.sleep(30)
+ 
